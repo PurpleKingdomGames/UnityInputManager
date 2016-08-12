be0282e2b3ab7a32b8912c83954d41dcab3eac4d
David North <hobnob@users.noreply.github.com>
2016-08-12 15:29:11 +0100

    Adds the input module as an option in menus

---
+++ Assets/Plugins/PurpleKingdomGames/InputManager/Editor/InputModuleEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PurpleKingdomGames.Unity.InputManager
+{
+    public static class InputModuleEditor
+    {
+        [MenuItem("GameObject/UI/Purple Kingdom Event System")]
+        private static void CreateInputModule()
+        {
+            if (Object.FindObjectOfType<EventSystem>() == null) {
+                GameObject obj = new GameObject();
+                obj.name = "EventSystem";
+                obj.AddComponent<EventSystem>();
+                obj.AddComponent<InputModule>();
+            }
+        }
+    }
+}
