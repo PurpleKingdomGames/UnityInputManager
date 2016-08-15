3d6b8c18f18cdd11b653b4698eb82e63eb61ce9b
David North <bigchimp2005+github@gmail.com>
2016-08-15 14:11:24 +0100

    Adds additional file checks and moves the menu to Tools

--- Assets/Plugins/PurpleKingdomGames/InputManager/Editor/InputModuleEditor.cs
+++ Assets/Plugins/PurpleKingdomGames/InputManager/Editor/InputModuleEditor.cs
@@ -7,6 +7,9 @@ namespace PurpleKingdomGames.Unity.InputManager
 {
     public static class InputModuleEditor
     {
+		private static string _inputPath = Application.dataPath + "/../ProjectSettings";
+		private static string _inputFile = "InputManager.asset";
+
         [MenuItem("GameObject/UI/Purple Kingdom Event System")]
         private static void CreateInputModule()
         {
@@ -18,36 +21,41 @@ namespace PurpleKingdomGames.Unity.InputManager
             }
         }
 
-		[MenuItem("Edit/Create Purple Kingdom Input File", false, 0)]
+		[MenuItem("Tools/Purple Kingdom Games/Create Input File", false, 0)]
 		private static void CreateInputConfig()
 		{
 			if (EditorUtility.DisplayDialog ("Replace Input Config?", "This action will overwrite your Input Config. Are you sure?", "Yes", "No")) {
 				string fromPath = Application.dataPath + "/Plugins/PurpleKingdomGames/InputManager/Editor";
 				string fromFilename = "InputManager.asset.config";
-				string toPath = Application.dataPath + "/../ProjectSettings";
-				string toFilename = "InputManager.asset";
 
 				EditorApplication.LockReloadAssemblies ();
-				File.Copy (toPath + "/" + toFilename, toPath + "/" + toFilename + ".pkbkup", true);
-				File.Copy (fromPath + "/" + fromFilename, toPath + "/" + toFilename, true);
+				File.Copy (_inputPath + "/" + _inputFile, _inputPath + "/" + _inputFile + ".pkbkup", true);
+				File.Copy (fromPath + "/" + fromFilename, _inputPath + "/" + _inputFile, true);
 				EditorApplication.UnlockReloadAssemblies ();
 			}
 		}
 
-		[MenuItem("Edit/Restore old Input File", false, 0)]
-		private static void CreateInputConfig()
+		[MenuItem("Tools/Purple Kingdom Games/Restore old Input File", false, 0)]
+		private static void RestoreInputConfig()
 		{
-			if (EditorUtility.DisplayDialog ("Replace Input Config?", "This action will overwrite your Input Config. Are you sure?", "Yes", "No")) {
-				string fromPath = Application.dataPath + "/Plugins/PurpleKingdomGames/InputManager/Editor";
-				string fromFilename = "InputManager.asset.config";
-				string toPath = Application.dataPath + "/../ProjectSettings";
-				string toFilename = "InputManager.asset";
-
+			if (EditorUtility.DisplayDialog ("Restore backup?", "This action restore your config to it's. Are you sure?", "Yes", "No")) {
 				EditorApplication.LockReloadAssemblies ();
-				File.Copy (toPath + "/" + toFilename, toPath + "/" + toFilename + ".pkbkup", true);
-				File.Copy (fromPath + "/" + fromFilename, toPath + "/" + toFilename, true);
+				File.Copy (_inputPath + "/" + _inputFile, _inputPath + "/" + _inputFile, true);
+				File.Delete (_inputPath + "/" + _inputFile);
 				EditorApplication.UnlockReloadAssemblies ();
 			}
+		}
+
+		[MenuItem("Tools/Purple Kingdom Games/Restore old Input File", true)]
+		private static bool ValidateRestoreConfig()
+		{
+			return File.Exists(_inputPath + "/" + _inputFile + ".pkbkup");
+		}
+
+		[MenuItem("Tools/Purple Kingdom Games/Create Input File", true)]
+		private static bool ValidateCreateConfig()
+		{
+			return !ValidateRestoreConfig ();
 		}
     }
 }
